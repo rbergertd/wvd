506c1eabf61cbf2ad05f784d248f3a569129c136
Ryan Berger <ryan.berger@techdata.com>
2019-04-03 14:14:14 -0400

    Update Application Publishing.ps1

--- Application Publishing.ps1
+++ Application Publishing.ps1
@@ -14,11 +14,13 @@ Get-RdsAppGroup williamtest wvd-pool
 Get-RdsStartMenuApp Williamtest wvd-pool Autocad
 
 #Publish Apps using AppAlias (autofills in paths, etc)
+#ie: New-RdsRemoteApp <TenantName> <PoolName> RemoteAppGrpName -Name "Application Display Name" -AppAlias AppAliasFromAboveCommand
+
 New-RdsRemoteApp Williamtest wvd-pool Autocad -Name "Inventor 2019" -AppAlias autodeskinventor2019english
 New-RdsRemoteApp Williamtest wvd-pool Autocad -Name "Autocad Mechanical 2019" -AppAlias autocad2019englishautocadmechanical
 New-RdsRemoteApp Williamtest wvd-pool Autocad -Name "Revit 2019" -AppAlias revit2019
 
-#Remove user from desktop group, assign to RemoteApp group
+#Remove user from desktop group, assign to RemoteApp group - user CANNOT be in both groups at once. You can either see the desktop, or the RemoteApps.
 Add-RdsAppGroupUser Williamtest wvd-pool "Desktop Application Group" -UserPrincipalName ryan.berger@thelendingside.com
 Add-RdsAppGroupUser Williamtest wvd-pool Autocad -UserPrincipalName ryan.berger@thelendingside.com
 
