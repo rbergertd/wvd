719eae284d4c8959aa8b1b45b8882bf69ab42243
Ryan Berger <ryan.berger@techdata.com>
2019-11-26 11:25:21 -0500

    Just working on shit

---
+++ scripts/Assign-Tenant-Creator.ps1
@@ -0,0 +1,13 @@
+﻿$username = 'adrian@tdsolutionfactory.onmicrosoft.com'
+$app_name = "Windows Virtual Desktop"
+$app_role_name = "TenantCreator"
+ 
+# Get the user to assign, and the service principal for the app to assign to
+$user = Get-AzureADUser -ObjectId "$username"
+$sp = Get-AzureADServicePrincipal -Filter "displayName eq '$app_name'" 
+$appRole = $sp.AppRoles | Where-Object { $_.DisplayName -eq $app_role_name } 
+ 
+# Assign the user to the app role
+New-AzureADUserAppRoleAssignment -ObjectId $user.ObjectId -PrincipalId $user.ObjectId -ResourceId $sp.ObjectId -Id $appRole.Id | out-null
+sleep 5
+ 
--- scripts/Create-WVD-Tenant.ps1
+++ scripts/Create-WVD-Tenant.ps1
@@ -1,9 +1,16 @@
-Install-Module -Name Microsoft.RDInfra.RDPowerShell
+$SubscriptionId = "fed52fef-daba-46ab-8a2c-2843fdbe947c"
+$AzureTenantId = "9e2e2052-0f49-4d74-891c-c36095f3af5e"
+$WVDTenantName = "Default Tenant"
+$User = "Ryan.berger@tdsolutionfactory.onmicrosoft.com"
+$Password = ConvertTo-SecureString -String "Sekur3P@$$Word" -AsPlainText -Force
+$Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $Password
 
+#Import Module
 Import-Module -Name Microsoft.RDInfra.RDPowerShell
 
-Add-RdsAccount -DeploymentUrl "https://rdbroker.wvd.microsoft.com"
+Add-RdsAccount -DeploymentUrl "https://rdbroker.wvd.microsoft.com" -Credential $Credential
 
-New-RdsTenant -Name Williamtest -AadTenantId 9e2e2052-0f49-4d74-891c-c36095f3af5e -AzureSubscriptionId fed52fef-daba-46ab-8a2c-2843fdbe947c
+New-RdsTenant -Name TestTenant -AadTenantId $AzureTenantId -AzureSubscriptionId $SubscriptionId
 
 
+ 
